942872885bc74f83be4ecb94d916d452e2ec468a
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-08-09 11:13:23 -0500

    feat: Add models and serializers for custom user

---
+++ t2t_api/app/models/custom_user.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
