63f0a28fc1937053ab4906a8e37b02e1ae89a4c9
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-08-15 17:59:12 -0500

    chore: Update database engine to use PostgreSQL instead of psycopg

--- t2t_api/config/settings.py
+++ t2t_api/config/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
