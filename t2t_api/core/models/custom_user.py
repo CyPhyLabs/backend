f692c0983d964db9f8fab2392eaa97d3defa1684
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-08-18 21:05:03 -0500

    feat: Add user_type choices to CustomUser model

--- t2t_api/core/models/custom_user.py
+++ t2t_api/core/models/custom_user.py
@@ -5,7 +5,8 @@ import uuid
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    user_type = models.CharField(max_length=50)
+    # user type can be either 'user' or 'staff'
+    user_type = models.CharField(max_length=50, choices=[('user', 'user'), ('staff', 'staff')], default='user')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
