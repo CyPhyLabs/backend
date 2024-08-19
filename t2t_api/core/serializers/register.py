ca6c76a044c974b983e18d7dd661170cfec8a75f
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-08-18 21:03:55 -0500

    feat: Add user_type field to registration API

--- t2t_api/core/serializers/register.py
+++ t2t_api/core/serializers/register.py
@@ -6,13 +6,14 @@ User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'user_type')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            user_type=validated_data['user_type']
         )
         return user
