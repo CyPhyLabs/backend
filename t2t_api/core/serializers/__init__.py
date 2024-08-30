14a181b6417fed33668fc2ab1b3619df93ba5ab5
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-08-30 16:26:12 -0500

    feat: Add device token serializer and API endpoint

--- t2t_api/core/serializers/__init__.py
+++ t2t_api/core/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .user import UserSerializer
 from .register import RegisterSerializer
 from .tokens import CustomTokenObtainPairSerializer, CustomTokenRefreshSerializer
+from .device_tokens import DeviceSerializer
