e8626086a9785b6b6a4b425019e64fefc42baeb0
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-08-30 16:25:20 -0500

    chore: Refactor API view imports

--- t2t_api/core/api/__init__.py
+++ t2t_api/core/api/__init__.py
@@ -1,5 +1,6 @@
-from .register import RegisterView
-from .logout import LogoutView
+from .register_view import RegisterView
+from .logout_view import LogoutView
 from .test_message_view import CreateMessageView
-from .tokens import CustomTokenObtainPairView, CustomTokenRefreshView
-from .testView import TestUserIDView
+from .auth_tokens_view import CustomTokenObtainPairView, CustomTokenRefreshView
+from .test_view import TestUserIDView
+from .device_tokens_view import DeviceTokenView
